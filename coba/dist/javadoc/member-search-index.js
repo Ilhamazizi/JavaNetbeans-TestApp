15fc3f426caab300729db7d87196f8886a016ff1
Ilhamazizi <muh.ilhamazizi@gmail.com>
2022-06-01 06:39:34 +0700

    TestAppNetbeans

---
+++ coba/dist/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.stmik","c":"Coba","l":"Coba()","url":"%3Cinit%3E()"},{"p":"com.stmik","c":"Coba","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ coba/src/com/stmik/Coba.java
@@ -0,0 +1,10 @@
+package com.stmik;
+
+public class Coba {
+
+      public static void main(String[] args) {
+       
+          
+    }
+    
+}
